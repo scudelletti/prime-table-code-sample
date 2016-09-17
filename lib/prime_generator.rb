eba79d6b54c62359e8a32a192822a402169d1c09
Diogo Scudelletti <diogo@scudelletti.com>
2016-09-17 11:54:09 +0200

    Extract Prime Generator logic to smaller instance methods

--- lib/prime_generator.rb
+++ lib/prime_generator.rb
@@ -1,16 +1,38 @@
 class PrimeGenerator
+  STARTER_NUMBER = 2
+
   def self.generate(number_of_primes)
-    accumulator = []
-    number = 1.next
+    new(number_of_primes).generate
+  end
 
+  private_class_method :new
+
+  def initialize(number_of_primes)
+    @number_of_primes = number_of_primes
+    @accumulator = []
+    @current_number = 2
+  end
+
+  def generate
     while accumulator.size != number_of_primes
-      if (2...number).none? { |n| number % n == 0 }
-        accumulator << number
-      end
-
-      number = number.next
+      accumulator << current_number if prime?
+      next_number
     end
 
     accumulator
+  end
+
+  private
+
+  attr_accessor :number_of_primes, :accumulator, :current_number
+
+  def prime?
+    (STARTER_NUMBER...current_number).none? do |n|
+      current_number % n == 0
+    end
+  end
+
+  def next_number
+    self.current_number = current_number.next
   end
 end
